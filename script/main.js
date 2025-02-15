53f8a44478ea3f4af0bb9f6954e2c726399b4445
Anna Inozemets <inoznetta@gmail.com>
2025-02-15 19:35:51 +0200

    Add more sections

--- script/main.js
+++ script/main.js
@@ -5,4 +5,11 @@ $(() => {
   $('#close-burger').on('click', () => {
     $('.header--mobile nav').removeClass('open')
   });
+  $('.sec5__slider').slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+  });
 })
